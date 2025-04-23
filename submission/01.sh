92bc07b241915db6836fbc72c90aa8097059ee76
Ogunseye Oluwajuwon <ogunseyemicheal@gmail.com>
2025-04-23 16:48:54 +0100

    add

--- submission/01.sh
+++ submission/01.sh
@@ -1,2 +1,7 @@
 # What is the hash of block 243,833?
-000000b024f11595795f0be5974229f786df389c8b92e92b61890e9e3c6b8d8a
+#000000b024f11595795f0be5974229f786df389c8b92e92b61890e9e3c6b8d8a
+#!/bin/bash
+
+# Get the hash of block 243833 from the Signet network
+bitcoin-cli -signet getblockhash 243833
+
