6e46020c5ba4bbc1d4fea6983e3fee9d2d9aec47
Ogunseye Oluwajuwon <ogunseyemicheal@gmail.com>
2025-04-23 18:01:05 +0100

    zeroKey

--- submission/04.sh
+++ submission/04.sh
@@ -1,10 +1,12 @@
 # Which public key signed input 0 in this tx: d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7
 #!/bin/bash
+#!/bin/bash
 
-# Transaction ID
-TXID="d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7"
+# Get the scriptSig for input 0
+SCRIPT_SIG=$(bitcoin-cli -signet getrawtransaction d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7 1 | jq -r '.vin[0].scriptSig.asm')
 
-# Extract and print the public key from input 0
-bitcoin-cli -signet getrawtransaction "$TXID" true \
-  | jq -r '.vin[0].scriptSig.asm' \
-  | awk '{print $2}'
+# Extract the public key (last element in scriptSig)
+PUBKEY=$(echo "$SCRIPT_SIG" | awk '{print $NF}')
+
+# Output the public key (for autograder)
+echo "$PUBKEY"
