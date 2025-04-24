f4458f623f32a54f57888e7d5f9bb8973c3719f9
Ogunseye Oluwajuwon <ogunseyemicheal@gmail.com>
2025-04-24 14:44:04 +0100

    nn

--- submission/07.sh
+++ submission/07.sh
@@ -1,1 +1,6 @@
 # what is the coinbase tx in this block 243,834
+#!/bin/bash
+
+BLOCK_HASH=$(bitcoin-cli -signet getblockhash 243834)
+COINBASE_TX=$(bitcoin-cli -signet getblock "$BLOCK_HASH" | jq -r '.tx[0]')
+echo "$COINBASE_TX"
