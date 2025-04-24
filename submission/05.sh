0e70a71c93b8beed4641a68a90e14c06c634e4bd
Ogunseye Oluwajuwon <ogunseyemicheal@gmail.com>
2025-04-24 13:35:11 +0100

    submit2

--- submission/05.sh
+++ submission/05.sh
@@ -5,34 +5,35 @@
 
 TXID="b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb"
 
-# Initialize variables
-INPUTS_SATS=0
-OUTPUTS_SATS=0
+# Function to convert BTC to satoshis
+btc_to_sats() {
+    echo "$1 * 100000000" | bc | awk '{printf "%.0f", $1}'
+}
 
-# Get transaction details
+# Get the transaction data
 TX_DATA=$(bitcoin-cli -signet getrawtransaction "$TXID" 1)
 
-# Sum inputs (UTXOs spent)
-for VIN in $(echo "$TX_DATA" | jq -r '.vin[] | .txid + " " + (.vout|tostring)'); do
-    TXID_PREV=$(echo "$VIN" | awk '{print $1}')
-    VOUT=$(echo "$VIN" | awk '{print $2}')
+# Calculate total outputs in satoshis
+OUTPUTS_SATS=$(echo "$TX_DATA" | jq -r '[.vout[].value] | add | . * 100000000 | floor')
+
+# Calculate total inputs in satoshis
+INPUTS_SATS=0
+for vin in $(echo "$TX_DATA" | jq -c '.vin[]'); do
+    prev_txid=$(echo "$vin" | jq -r '.txid')
+    vout=$(echo "$vin" | jq -r '.vout')
     
-    # Skip coinbase inputs (they have empty txid)
-    if [[ "$TXID_PREV" == "0000000000000000000000000000000000000000000000000000000000000000" ]]; then
+    # Skip if coinbase input (null txid)
+    if [ "$prev_txid" == "null" ]; then
         continue
     fi
     
-    PREV_TX_DATA=$(bitcoin-cli -signet getrawtransaction "$TXID_PREV" 1)
-    VALUE=$(echo "$PREV_TX_DATA" | jq -r ".vout[$VOUT].value")
-    VALUE_SATS=$(echo "$VALUE * 100000000" | bc | awk '{printf "%.0f", $1}')
-    INPUTS_SATS=$((INPUTS_SATS + VALUE_SATS))
+    prev_tx_data=$(bitcoin-cli -signet getrawtransaction "$prev_txid" 1)
+    value=$(echo "$prev_tx_data" | jq -r ".vout[$vout].value")
+    INPUTS_SATS=$((INPUTS_SATS + $(btc_to_sats "$value")))
 done
 
-# Sum outputs (recipient amounts)
-OUTPUTS_SATS=$(echo "$TX_DATA" | jq -r '[.vout[].value] | add * 100000000 | floor')
-
-# Calculate fee (inputs - outputs)
+# Calculate fee
 FEE_SATS=$((INPUTS_SATS - OUTPUTS_SATS))
 
-# Print ONLY the fee in satoshis
+# Output just the fee amount
 echo "$FEE_SATS"
