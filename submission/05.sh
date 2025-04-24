9fdeb0e9978449d3af7e5d0f4a7b4393ecfcb46e
Ogunseye Oluwajuwon <ogunseyemicheal@gmail.com>
2025-04-24 14:35:28 +0100

    bbbb

--- submission/05.sh
+++ submission/05.sh
@@ -4,31 +4,28 @@
 
 TXID="b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb"
 
-# Get transaction with full details
-TX_DATA=$(bitcoin-cli -signet getrawtransaction "$TXID" 1)
+# Fetch full transaction with input info
+TX_JSON=$(bitcoin-cli -signet getrawtransaction "$TXID" true)
 
-# Calculate total outputs in satoshis (truncated)
-OUTPUTS=$(echo "$TX_DATA" | jq -r '[.vout[].value] | add * 100000000 | floor')
-
-# Calculate total inputs in satoshis
-INPUTS=0
-for vin in $(echo "$TX_DATA" | jq -c '.vin[]'); do
-    prev_txid=$(echo "$vin" | jq -r '.txid')
-    vout=$(echo "$vin" | jq -r '.vout')
-    
-    # Skip coinbase inputs
-    if [ "$prev_txid" != "null" ]; then
-        # Get previous output value
-        prev_tx_data=$(bitcoin-cli -signet getrawtransaction "$prev_txid" 1)
-        value=$(echo "$prev_tx_data" | jq -r ".vout[$vout].value")
-        # Convert to satoshis and add (using awk for reliable conversion)
-        value_sats=$(echo "$value" | awk '{printf "%.0f", $1 * 100000000}')
-        INPUTS=$((INPUTS + value_sats))
-    fi
+# Step 1: Calculate total output
+TOTAL_OUTPUT=0
+for value in $(echo "$TX_JSON" | jq -r '.vout[].value'); do
+  SATOSHIS=$(echo "$value * 100000000" | bc | cut -d'.' -f1)
+  TOTAL_OUTPUT=$((TOTAL_OUTPUT + SATOSHIS))
 done
 
-# Calculate fee (inputs - outputs)
-FEE=$((INPUTS - OUTPUTS))
+# Step 2: Calculate total input
+TOTAL_INPUT=0
+for i in $(echo "$TX_JSON" | jq -c '.vin[]'); do
+  VIN_TXID=$(echo "$i" | jq -r '.txid')
+  VIN_VOUT=$(echo "$i" | jq -r '.vout')
+  INPUT_TX=$(bitcoin-cli -signet getrawtransaction "$VIN_TXID" true)
+  VALUE=$(echo "$INPUT_TX" | jq -r ".vout[$VIN_VOUT].value")
+  SATOSHIS=$(echo "$VALUE * 100000000" | bc | cut -d'.' -f1)
+  TOTAL_INPUT=$((TOTAL_INPUT + SATOSHIS))
+done
 
-# Print only the fee amount with no extra characters
+# Step 3: Fee = input - output
+FEE=$((TOTAL_INPUT - TOTAL_OUTPUT))
+
 echo $FEE
