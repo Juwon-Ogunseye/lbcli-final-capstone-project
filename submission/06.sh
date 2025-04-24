215b0f81f71409f2bb322039b13c33d39d657a4e
Ogunseye Oluwajuwon <ogunseyemicheal@gmail.com>
2025-04-24 14:39:11 +0100

    six

--- submission/06.sh
+++ submission/06.sh
@@ -1,1 +1,13 @@
 # Only one tx in block 243,821 signals opt-in RBF. What is its txid?
+#!/bin/bash
+
+BLOCK_HASH=$(bitcoin-cli -signet getblockhash 243821)
+TXIDS=$(bitcoin-cli -signet getblock "$BLOCK_HASH" | jq -r '.tx[]')
+
+for TXID in $TXIDS; do
+  SEQ=$(bitcoin-cli -signet getrawtransaction "$TXID" true | jq '.vin[].sequence')
+  if echo "$SEQ" | grep -qv "4294967295"; then
+    echo "$TXID"
+    exit 0
+  fi
+done
